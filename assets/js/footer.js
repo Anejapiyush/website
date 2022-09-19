cce6eed89c8a9e208dfec6e6729e53f9fa0c6766
Piyush Aneja <103268377+Anejapiyush@users.noreply.github.com>
2022-09-19 17:43:07 +0530

    Add files via upload

---
+++ assets/js/footer.js
@@ -0,0 +1,15 @@
+
+
+
+fetch('../../footer.html')
+  .then(response => response.text())
+  .then(text => {
+      let footer = document.getElementById('footer');
+
+      footer.innerHTML = text;
+  })
+
+
+  
+
+  
